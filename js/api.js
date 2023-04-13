b90b115bcc8212776f556a4f7acbe1c454295227
Paddy Rudden <ruddenpatrick@gmail.com>
2023-04-13 17:20:12 +0100

    github api repos added and styled

--- js/api.js
+++ js/api.js
@@ -1,17 +1,46 @@
 function avatar(user) {
-   const avatar = document.createElement('img');
+    const avatar = document.createElement('img');
     avatar.alt = `%{user.login}'s avatar`;
     avatar.src = user.avatar_url;
     return avatar;
-}
+  }
 
-
-async function getUser(username) {
-    const response = await fetch(`https://api.github.com/users/${username}`);
+  async function getJSON(url) {
+    const response = await fetch(url);
     return response.json()
-}
+  }
 
-getUser('PaddyFRGitHub').then(user => {
+  async function getUser(username) {
+    return getJSON(`https://api.github.com/users/${username}`);
+  }
+
+  async function gitStuff() {
+    const user = await getUser('PaddyFRGitHub');
+    console.log(user);
+    const repos = await getJSON(user.repos_url);
+    console.log(repos);
+
+    // Retrieve the container element from the HTML document
+    const ghapi = document.getElementById('ghapi');
+
+    // Create and append the user's avatar to the container element
     const a = avatar(user);
-    ghapi.append(a);
-});
+    ghapi.appendChild(a);
+
+    // Create a list of the user's repositories
+    const repoList = document.createElement('ul');
+    repos.forEach(repo => {
+      const repoLink = document.createElement('a');
+      repoLink.href = repo.html_url;
+      repoLink.textContent = repo.name;
+
+      const repoListItem = document.createElement('li');
+      repoListItem.appendChild(repoLink);
+      repoList.appendChild(repoListItem);
+    });
+
+    // Append the list of repositories to the container element
+    ghapi.appendChild(repoList);
+  }
+
+  gitStuff();
